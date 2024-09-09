f881ce8631e1663750bfc632b5b10dfc158e4257
Dylan Mendonca <dylan@phenomic.ai>
2024-09-09 14:50:09 -0400

    soma data package uploaded

--- soma_data/src/soma_data/__init__.py
+++ soma_data/src/soma_data/__init__.py
@@ -5,11 +5,6 @@ try:
 except PackageNotFoundError:
     __version__ = "0.0.0"
 
-__author__ = "Phenomic AI"
-__email__ = "sctx@phenomic.ai"
-__license__ = "MIT"
-__description__ = "A class to handle data from a Soma experiment"
-
 from .soma_data import SomaData
 
 __all__ = ["SomaData"]
