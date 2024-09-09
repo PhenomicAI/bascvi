ef9f83ba179ba3e7661e393cf6f4c747a2440233
sam cooper <samcooper@sams-MacBook-Pro.local>
2024-09-09 14:22:05 -0400

    rebase

--- bascvi/bascvi/datamodule/__init__.py
+++
@@ -1,5 +0,0 @@
-#from .datamodule import ScRNASeqDataModule
-#from .datamodule import ScRNASeqTestDataModule
-from .anndata.datamodule import AnnDataDataModule
-from .soma.datamodule import TileDBSomaIterDataModule
-from .embeddings.datamodule import EmbDatamodule
