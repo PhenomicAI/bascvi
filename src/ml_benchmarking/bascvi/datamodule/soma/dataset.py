727b4b32333bb107c8f0e94e9eecdfc82fecf5b0
Ubuntu <ubuntu@ip-172-31-34-174.us-east-2.compute.internal>
2025-07-22 16:32:47 +0000

    cleanup

--- src/ml_benchmarking/bascvi/datamodule/soma/dataset.py
+++ src/ml_benchmarking/bascvi/datamodule/soma/dataset.py
@@ -5,6 +5,7 @@ import torch
 import numpy as np
 import tiledbsoma as soma
 from torch.utils.data import IterableDataset
+import torch.nn.functional as F
 
 from ml_benchmarking.bascvi.datamodule.soma.soma_helpers import open_soma_experiment
 
@@ -31,6 +32,7 @@ class TileDBSomaTorchIterDataset(IterableDataset):
         predict_mode: bool = False,
         pretrained_gene_indices: Optional[np.ndarray] = None
     ) -> None:
+
         self.soma_experiment_uri = soma_experiment_uri
         self.obs_df = obs_df
         self.genes_to_use = genes_to_use
@@ -47,27 +49,25 @@ class TileDBSomaTorchIterDataset(IterableDataset):
         self.verbose = verbose
         self.pretrained_gene_indices = pretrained_gene_indices
 
-        #Cache info on obs_df
         with open_soma_experiment(self.soma_experiment_uri) as soma_experiment:
-            self.rna_X = soma_experiment.ms["RNA"]["X"][self.X_array_name]
             self.full_shape = (
                 soma_experiment.obs.count,
                 soma_experiment.ms["RNA"].var.count
             )
-    
-        # Validate input DataFrame
         assert self.obs_df.soma_joinid.nunique() == self.obs_df.shape[0], "soma_joinid must be unique per row."
         assert self.obs_df.cell_idx.nunique() == self.obs_df.shape[0], "cell_idx must be unique per row."
+        
         if self.predict_mode:
             assert self.num_modalities is None
             assert self.num_studies is None
             assert self.num_samples is None
-
-        # Block and cell counters for iteration
+            
         self.block_counter = 0
         self.cell_counter = 0
         self.block_size = block_size
         self.num_blocks = math.ceil(self.obs_df.shape[0] / self.block_size)
+        # For block data
+        self._block_data = None
 
     def __len__(self) -> int:
         return self._len
@@ -96,6 +96,7 @@ class TileDBSomaTorchIterDataset(IterableDataset):
             self.end_block = self.num_blocks
         self.block_counter = 0
         self.cell_counter = 0
+        self._block_data = None
         if self.verbose:
             print("start block, end block, block_size: ", self.start_block, self.end_block, self.block_size)
         return self
@@ -106,31 +107,31 @@ class TileDBSomaTorchIterDataset(IterableDataset):
         if self.verbose:
             print("reading new block of size ", end_idx - start_idx)
         obs_df_block = self.obs_df.iloc[start_idx:end_idx, : ]
+
         cell_idx_block = obs_df_block['cell_idx'].to_numpy(dtype=np.int64)
         soma_joinid_block = obs_df_block["soma_joinid"].to_numpy(dtype=np.int64)
         modality_idx_block = obs_df_block["modality_idx"].to_numpy()
         study_idx_block = obs_df_block["study_idx"].to_numpy()
         sample_idx_block = obs_df_block["sample_idx"].to_numpy()
+
         assert obs_df_block.shape[0] == (end_idx - start_idx)
         assert len(np.unique(soma_joinid_block)) == (end_idx - start_idx)
         assert len(np.unique(cell_idx_block)) == (end_idx - start_idx)
         try:
             with open_soma_experiment(self.soma_experiment_uri) as soma_experiment:
                 sorted_soma_joinids = sorted(soma_joinid_block)
-
+                rna_X = soma_experiment.ms["RNA"]["X"][self.X_array_name]
                 sparse_matrix = (
-                    self.rna_X
+                    rna_X
                     .read((tuple(sorted_soma_joinids), None))
                     .coos(shape=self.full_shape)
                     .concat()
                     .to_scipy()
                     .tocsr()
                 )
-
                 X_block = sparse_matrix[soma_joinid_block, :]
                 nan_mask = np.isnan(X_block.data)
                 X_block.data[nan_mask] = 0
-
             X_block = X_block[:, self.genes_to_use]
         except Exception as error:
             print("Error reading X array of block: ", curr_block)
@@ -147,23 +148,23 @@ class TileDBSomaTorchIterDataset(IterableDataset):
         }
         if self.predict_mode:
             return base
-        # Add training/validation fields
-        one_hot_modality = np.zeros((self.num_modalities,), dtype=np.float32)
-        one_hot_study = np.zeros((self.num_studies,), dtype=np.float32)
-        one_hot_sample = np.zeros((self.num_samples,), dtype=np.float32)
-        one_hot_modality[modality_idx_curr] = 1
-        one_hot_study[study_idx_curr] = 1
-        one_hot_sample[sample_idx_curr] = 1
+
+        # Use torch.nn.functional.one_hot for one-hot encoding, ensure dtype is torch.long
+        one_hot_modality = F.one_hot(torch.tensor(modality_idx_curr, dtype=torch.long), num_classes=self.num_modalities).float()
+        one_hot_study = F.one_hot(torch.tensor(study_idx_curr, dtype=torch.long), num_classes=self.num_studies).float()
+        one_hot_sample = F.one_hot(torch.tensor(sample_idx_curr, dtype=torch.long), num_classes=self.num_samples).float()
+        
         if sample_idx_curr in self.library_calcs.index:
             local_l_mean = self.library_calcs.loc[sample_idx_curr, "library_log_means"]
             local_l_var = self.library_calcs.loc[sample_idx_curr, "library_log_vars"]
         else:
             local_l_mean = 0.0
             local_l_var = 1.0
+
         base.update({
-            "modality_vec": torch.from_numpy(one_hot_modality),
-            "study_vec": torch.from_numpy(one_hot_study),
-            "sample_vec": torch.from_numpy(one_hot_sample),
+            "modality_vec": one_hot_modality,
+            "study_vec": one_hot_study,
+            "sample_vec": one_hot_sample,
             "local_l_mean": torch.tensor(local_l_mean),
             "local_l_var": torch.tensor(local_l_var),
         })
@@ -175,37 +176,39 @@ class TileDBSomaTorchIterDataset(IterableDataset):
         self.curr_block = self.start_block + self.block_counter 
         if self.curr_block < self.end_block:
             if self.cell_counter == 0:
-                (
-                    self.obs_df_block,
-                    self.cell_idx_block,
-                    self.soma_joinid_block,
-                    self.modality_idx_block,
-                    self.study_idx_block,
-                    self.sample_idx_block,
-                    self.X_block
-                ) = self._get_block(self.curr_block)
+                self._block_data = self._get_block(self.curr_block)
+            (
+                obs_df_block,
+                cell_idx_block,
+                soma_joinid_block,
+                modality_idx_block,
+                study_idx_block,
+                sample_idx_block,
+                X_block
+            ) = self._block_data
             if self.verbose:    
                 print("subsetting, converting, and transposing x...")
-            X_curr = np.squeeze(np.transpose(self.X_block[self.cell_counter, :].toarray()))
+            X_curr = np.squeeze(np.transpose(X_block[self.cell_counter, :].toarray()))
             if self.pretrained_gene_indices is not None:
                 X_curr_full = np.zeros(self.num_input,  dtype=np.int32)
                 X_curr_full[self.pretrained_gene_indices] = X_curr
                 X_curr = np.squeeze(np.transpose(X_curr_full))
-            sample_idx_curr = self.sample_idx_block[self.cell_counter]
-            soma_joinid = self.soma_joinid_block[self.cell_counter]
-            cell_idx = self.cell_idx_block[self.cell_counter]
+            sample_idx_curr = sample_idx_block[self.cell_counter]
+            soma_joinid = soma_joinid_block[self.cell_counter]
+            cell_idx = cell_idx_block[self.cell_counter]
             feature_presence_mask = self.feature_presence_matrix[sample_idx_curr, :]
             if self.predict_mode:
                 datum = self._make_datum(X_curr, soma_joinid, cell_idx, feature_presence_mask, sample_idx_curr)
             else:
-                modality_idx_curr = self.modality_idx_block[self.cell_counter]
-                study_idx_curr = self.study_idx_block[self.cell_counter]
+                modality_idx_curr = modality_idx_block[self.cell_counter]
+                study_idx_curr = study_idx_block[self.cell_counter]
                 datum = self._make_datum(
                     X_curr, soma_joinid, cell_idx, feature_presence_mask, sample_idx_curr, modality_idx_curr, study_idx_curr
                 )
-            if (self.cell_counter + 1) == self.obs_df_block.shape[0]:
+            if (self.cell_counter + 1) == obs_df_block.shape[0]:
                 self.block_counter += 1
                 self.cell_counter = 0
+                self._block_data = None
             else:
                 self.cell_counter += 1
             return datum
--- src/ml_benchmarking/scripts/train.py
+++ src/ml_benchmarking/scripts/train.py
@@ -10,152 +10,97 @@ from pytorch_lightning import Trainer
 
 from ml_benchmarking.bascvi.datamodule import TileDBSomaIterDataModule, AnnDataDataModule, EmbDatamodule
 from ml_benchmarking.bascvi.datamodule.soma.soma_helpers import open_soma_experiment
-from ml_benchmarking.bascvi.utils.utils import calc_kni_score, calc_rbni_score #, umap_calc_and_save_html
-
+from ml_benchmarking.bascvi.utils.utils import calc_kni_score, calc_rbni_score
 
 logger = logging.getLogger("pytorch_lightning")
 
-
-def train(config: Dict):
-    
-    if "wandb_project_name" not in config:
-        config["wandb_project_name"] = "bascvi"
-
-    # Initialize Wandb Logger
-    wandb_logger = WandbLogger(project=config["wandb_project_name"], save_dir=config["run_save_dir"])
-    wandb.init(project=config["wandb_project_name"], dir=config["run_save_dir"], config=config)
-
-    # for tiledb
-    torch.multiprocessing.set_start_method("fork", force=True)
-    orig_start_method = torch.multiprocessing.get_start_method()
-    if orig_start_method != "spawn":
-        if orig_start_method:
-            print(
-                "switching torch multiprocessing start method from "
-                f'"{torch.multiprocessing.get_start_method()}" to "spawn"'
-            )
-        torch.multiprocessing.set_start_method("spawn", force=True)
-
-
-    if config["datamodule"]["class_name"] == "TileDBSomaIterDataModule":
-        config["datamodule"]["options"]["root_dir"] = config["run_save_dir"]
-        datamodule = TileDBSomaIterDataModule(**config["datamodule"]["options"])
-
-    elif config["datamodule"]["class_name"] == "EmbDatamodule":
-        config["datamodule"]["options"]["root_dir"] = config["run_save_dir"]
-        datamodule = EmbDatamodule(**config["datamodule"]["options"])
-
-    elif config["datamodule"]["class_name"] == "AnnDataDataModule":
+def get_datamodule(config):
+    """Factory for datamodule based on config."""
+    options = config["datamodule"]["options"]
+    options["root_dir"] = config["run_save_dir"]
+    class_name = config["datamodule"]["class_name"]
+    if class_name == "TileDBSomaIterDataModule":
+        return TileDBSomaIterDataModule(**options)
+    elif class_name == "EmbDatamodule":
+        return EmbDatamodule(**options)
+    elif class_name == "AnnDataDataModule":
         raise NotImplementedError("Training with AnnDataDataModule is not implemented yet")
-        datamodule = AnnDataDataModule(**config["datamodule"]["options"])
+    else:
+        raise ValueError(f"Unknown datamodule class: {class_name}")
 
-    datamodule.setup()
-
-    # set the model gene list from the datamodule
+def get_trainer_and_model(config, datamodule, wandb_logger):
+    """Initialize model and trainer from config."""
     config['emb_trainer']['gene_list'] = datamodule.gene_list
-
-    # set the number of input genes and batches in the model from the datamodule
     config['emb_trainer']['model_args']['n_input'] = datamodule.num_genes
     config['emb_trainer']['model_args']['batch_level_sizes'] = datamodule.batch_level_sizes
-
     config["emb_trainer"]["soma_experiment_uri"] = datamodule.soma_experiment_uri
-
-    # dynamically import trainer class
-    module = __import__("ml_benchmarking.bascvi.trainer", globals(), locals(), [config["trainer_module_name"] if "trainer_module_name" in config else "bascvi_trainer"], 0)
-    EmbeddingTrainer = getattr(module, config["trainer_class_name"] if "trainer_class_name" in config else "BAScVITrainer")
-
+    module = __import__(
+        "ml_benchmarking.bascvi.trainer",
+        globals(), locals(),
+        [config.get("trainer_module_name", "bascvi_trainer")], 0)
+    EmbeddingTrainer = getattr(module, config.get("trainer_class_name", "BAScVITrainer"))
     if config.get("load_from_checkpoint"):
         logger.info(f"Loading trainer from checkpoint.....")
-        model = EmbeddingTrainer.load_from_checkpoint(
-            config["load_from_checkpoint"], 
-            )
+        model = EmbeddingTrainer.load_from_checkpoint(config["load_from_checkpoint"])
     else:
         logger.info(f"Initializing Custom Embedding Trainer.....")
-        model = EmbeddingTrainer(
-            config["run_save_dir"],
-            **config["emb_trainer"]
-            )
-    # add callbacks to pytroch lightning trainer config
+        model = EmbeddingTrainer(config["run_save_dir"], **config["emb_trainer"])
     if "pl_trainer" not in config:
         config["pl_trainer"] = {}
     config["pl_trainer"]["callbacks"] = model.callbacks
-
     model.datamodule = datamodule
+    trainer = Trainer(
+        **config["pl_trainer"],
+        logger=wandb_logger,
+        accelerator="gpu",
+        devices=1,
+        num_sanity_val_steps=2
+    )
+    return trainer, model
 
-    # logger.info(f"Initializing pytorch-lightning trainer.....")
-    trainer = Trainer(**config["pl_trainer"], logger=wandb_logger, accelerator="gpu", devices=1, num_sanity_val_steps=2)
-    #trainer.save_checkpoint("latest.ckpt")
-
-    # logger.addHandler(logging.FileHandler(os.path.join(cfg["datamodule"]["root_dir"], "std.log")))
-
-
-    logger.info("-----------------------Starting training-----------------------")
-    trainer.fit(model, datamodule=datamodule) # Hit ctrl C to trigger predict as soon as training starts - Hack but avoids a lot of extra code
-    logger.info(f"Best model path: {trainer.checkpoint_callback.best_model_path}")
-    logger.info(f"Best model score: {trainer.checkpoint_callback.best_model_score}")
-
-    trainer.save_checkpoint(os.path.join(os.path.dirname(trainer.checkpoint_callback.best_model_path), "latest.ckpt"))
-
-
-    # load the best checkpoint automatically (tracked by lightning itself)
-    logger.info("--------------Embedding prediction on full dataset-------------")
-
-
-    # if config["datamodule_class_name"] == "TileDBSomaIterDataModule":
-    #     config["datamodule"]["root_dir"] = cfg["run_save_dir"]
-    #     datamodule = TileDBSomaIterDataModule(**cfg["datamodule"])
-    # elif config["datamodule_class_name"] == "EmbDatamodule":
-    #     datamodule = EmbDatamodule(**cfg["datamodule"])
-
-    datamodule.pretrained_batch_size = datamodule.num_batches
-    datamodule.setup(stage="predict")
-
-    predictions = trainer.predict(model, datamodule=datamodule)
+def save_embeddings_and_metrics(trainer, datamodule, config, predictions):
     embeddings = torch.cat(predictions, dim=0).detach().cpu().numpy()
-
-    emb_columns = ["embedding_" + str(i) for i in range(embeddings.shape[1] - 1)] 
+    emb_columns = [f"embedding_{i}" for i in range(embeddings.shape[1] - 1)]
     embeddings_df = pd.DataFrame(data=embeddings, columns=emb_columns + ["soma_joinid"])
-
     logger.info("--------------------------Run UMAP----------------------------")
     with open_soma_experiment(datamodule.soma_experiment_uri) as soma_experiment:
         obs_df = soma_experiment.obs.read(
-                            column_names=("soma_joinid", "standard_true_celltype", "sample_name", "study_name", "barcode"),
-                        ).concat().to_pandas()
-    obs_df
+            column_names=("soma_joinid", "standard_true_celltype", "sample_name", "study_name", "barcode"),
+        ).concat().to_pandas()
     embeddings_df = embeddings_df.set_index("soma_joinid").join(obs_df.set_index("soma_joinid"))
-
     obs_df = obs_df.set_index("soma_joinid").loc[embeddings_df.index]
-    
-    # embeddings_df, fig_save_dict = umap_calc_and_save_html(embeddings_df, emb_columns, trainer.default_root_dir)
-
-    save_path = os.path.join(config["run_save_dir"], "pred_embeddings_" + os.path.splitext(os.path.basename(trainer.checkpoint_callback.best_model_path))[0] + ".tsv")
+    save_path = os.path.join(
+        config["run_save_dir"],
+        "pred_embeddings_" + os.path.splitext(os.path.basename(trainer.checkpoint_callback.best_model_path))[0] + ".tsv"
+    )
     embeddings_df.to_csv(save_path, sep="\t")
     logger.info(f"Saved predicted embeddings to: {save_path}")
-
-    # run metrics on the embeddings, and log to wandb
     logger.info("--------------------------Run Metrics----------------------------")
     kni_score = calc_kni_score(embeddings_df[emb_columns], obs_df)
     rbni_score = calc_rbni_score(embeddings_df[emb_columns], obs_df)
     logger.info(f"KNI Score: {kni_score}")
     logger.info(f"RBNI Score: {rbni_score}")
-
-    # plot confusion matrix
-    confusion_matrix = kni_score["confusion_matrix"]
-    # wandb.log({"confusion_matrix": wandb.plot.confusion_matrix(confusion_matrix, class_names=confusion_matrix.index)})
-
-    # drop the confusion matrix from the kni_score dict
-    kni_score.pop("confusion_matrix")
-    kni_score.pop("kni_confusion_matrix")
-    kni_score.pop("results_by_batch")
-    # kni_score.pop("non_diverse")
-    # kni_score.pop("non_diverse_correctly_predicted")
-    # kni_score.pop("non_diverse_incorrectly_predicted")
-
-    rbni_score.pop("results_by_batch")
-
-
+    for k in ["confusion_matrix", "kni_confusion_matrix", "results_by_batch"]:
+        kni_score.pop(k, None)
+    rbni_score.pop("results_by_batch", None)
     wandb.run.summary.update(kni_score)
     wandb.run.summary.update(rbni_score)
-
-    # end the wandb run
     wandb.finish()
+
+def train(config: Dict):
+    config.setdefault("wandb_project_name", "bascvi")
+    wandb_logger = WandbLogger(project=config["wandb_project_name"], save_dir=config["run_save_dir"])
+    wandb.init(project=config["wandb_project_name"], dir=config["run_save_dir"], config=config)
+    datamodule = get_datamodule(config)
+    datamodule.setup()
+    trainer, model = get_trainer_and_model(config, datamodule, wandb_logger)
+    logger.info("-----------------------Starting training-----------------------")
+    trainer.fit(model, datamodule=datamodule)
+    logger.info(f"Best model path: {trainer.checkpoint_callback.best_model_path}")
+    logger.info(f"Best model score: {trainer.checkpoint_callback.best_model_score}")
+    trainer.save_checkpoint(os.path.join(os.path.dirname(trainer.checkpoint_callback.best_model_path), "latest.ckpt"))
+    logger.info("--------------Embedding prediction on full dataset-------------")
+    datamodule.pretrained_batch_size = datamodule.num_batches
+    datamodule.setup(stage="predict")
+    predictions = trainer.predict(model, datamodule=datamodule)
+    save_embeddings_and_metrics(trainer, datamodule, config, predictions)
